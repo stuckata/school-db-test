ecf729a8dff1ded0b458238ffb045d9cf101e023
Stoyan Kirkov <stu.kirkov@gmail.com>
2018-12-21 22:36:47 +0200

    initial

---
+++ src/main/java/com/stoyankirkov/schooldbtest/SchoolDbTestApplication.java
@@ -0,0 +1,14 @@
+package com.stoyankirkov.schooldbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolDbTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolDbTestApplication.class, args);
+	}
+
+}
+
